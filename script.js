4b00f10bdff1f83f6c797f7443c10fa9c3020d05
annayemsen <97625734+annayemsen@users.noreply.github.com>
2023-08-24 00:17:39 +0300

    added responsive navbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector("svg");
 menu.addEventListener("click", morph);
 const nav = document.querySelector(".navigation");
-
 function morph() {
   menu.classList.toggle("open");
   nav.classList.toggle("active");
