7c331ac41dec958481b80fbf6320e02284abf470
annayemsen <97625734+annayemsen@users.noreply.github.com>
2023-08-24 22:05:25 +0300

    finished homepage

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector("svg");
 menu.addEventListener("click", morph);
 const nav = document.querySelector(".navigation");
+const body = document.querySelector("body");
 function morph() {
   menu.classList.toggle("open");
   nav.classList.toggle("active");
+  body.classList.toggle("fixed");
 }
